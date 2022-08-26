d30843e00fb14f07d2a5cd5421e25404d13c8e81
Sandro Rassy <sandrorassy@gmail.com>
2022-08-26 11:45:39 -0300

    Classe base de testes

---
+++ Layer.UnitTests/System/Base/TestBase.cs
@@ -0,0 +1,41 @@
+﻿using Layer.API.Models.Enum;
+using Layer.Domain.Settings;
+using Layer.Repository;
+using Layer.Repository.Context;
+using Layer.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.UnitTests.System.Base
+{
+    public class TestBase
+    {
+        public static IConfiguration Configuration { get; }
+        public readonly MongoDBSetting mongoDbSettings;
+        public readonly ConnectionFactory connectionFactory;
+        public readonly UsuarioRepository _usuarioRepository;
+        public readonly UsuarioPosicaoRepository _usuarioPosicaoRepository;
+        public readonly TendenciaRepository _tendenciaRepository;
+        public readonly UsuarioServices _usuarioServices;
+        public readonly UsuarioPosicaoServices _usuarioPosicaoServices;
+        public readonly TendenciaServices _tendenciaServices;
+
+        public TestBase()
+        {
+            mongoDbSettings = Configuration.GetSection("MongoDatabase").Get<MongoDBSetting>();
+            connectionFactory = new ConnectionFactory(mongoDbSettings.ConnectionString);
+
+            _usuarioRepository = new UsuarioRepository(connectionFactory, mongoDbSettings.DatabaseName, MongoDBCollections.CNUsuarios.ToString());
+            _usuarioPosicaoRepository = new UsuarioPosicaoRepository(connectionFactory, mongoDbSettings.DatabaseName, MongoDBCollections.CNUsuariosPosicao.ToString());
+            _tendenciaRepository = new TendenciaRepository(connectionFactory, mongoDbSettings.DatabaseName, MongoDBCollections.CNTendencias.ToString());
+
+            _usuarioServices = new UsuarioServices(_usuarioRepository);
+            _usuarioPosicaoServices = new UsuarioPosicaoServices(_usuarioPosicaoRepository);
+            _tendenciaServices = new TendenciaServices(_tendenciaRepository);
+        }
+    }
+}
---
+++ Layer.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Layer.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
