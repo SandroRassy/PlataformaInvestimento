ed43dc39c5ab35f47c32d486c0a529fb74160528
Sandro Rassy <sandrorassy@gmail.com>
2022-08-26 17:02:31 -0300

    Controller usuario - teste

---
+++ Layer.UnitTests/System/Controllers/TesteUsuarioController.cs
@@ -0,0 +1,46 @@
+﻿using Layer.API.Controllers;
+using Layer.API.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.UnitTests.System.Controllers
+{
+    public class TesteUsuarioController : TestBase
+    {
+        [Fact]
+        public async Task GetAll_Return()
+        {
+            /// Arrange            
+            var sut = new UsuarioController(_usuarioServices);
+
+            /// Act
+            var result = (ObjectResult)sut.Get();
+
+            /// Assert
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task Post_Cpf_ShouldReturn400Status()
+        {
+            /// Arrange            
+            var sut = new UsuarioController(_usuarioServices);
+
+            /// Act
+            var obj = new UsuarioDto();
+
+            obj.CPF = "012";
+            obj.CodigoConta = "1010";
+            obj.Nome = "teste";
+
+            var result = (ObjectResult)sut.Post(obj);
+
+            /// Assert
+            result.StatusCode.Should().Be(400);
+        }
+    }
+}
