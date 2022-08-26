a546f7d721fa66f53d73f5200756719ba2af712a
Sandro Rassy <sandrorassy@gmail.com>
2022-08-26 15:49:46 -0300

    UsuarioController

---
+++ Layer.API/Models/DTO/UsuarioDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Layer.API.Models.DTO
+{
+    public class UsuarioDto
+    {
+        public string Nome { get; set; }
+        public string CodigoConta { get; set; }
+        public string CPF { get; set; }
+    }
+}
--- Layer.Services/Interfaces/IUsuarioService.cs
+++ Layer.Services/Interfaces/IUsuarioService.cs
@@ -10,5 +10,7 @@ namespace Layer.Services.Interfaces
     public interface IUsuarioService : IService<Usuario>
     {
         Usuario QueryFilter(string cpf);
+
+        void Inserir(string nome, string codigoconta, string cpf);
     }
 }
