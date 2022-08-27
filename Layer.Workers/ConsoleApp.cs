724f0cabbfc0957e66c8c0bf46256fbe259c2312
Sandro Rassy <sandrorassy@gmail.com>
2022-08-27 00:14:46 -0300

    Worker

---
+++ Layer.Workers/ConsoleApp.cs
@@ -0,0 +1,56 @@
+﻿using Layer.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Layer.Workers
+{
+    public class ConsoleApp
+    {
+        private readonly IUsuarioPosicaoRepository _usuarioPosicaoRepository;
+        public ConsoleApp(IUsuarioPosicaoRepository usuarioPosicaoRepository)
+        {
+            _usuarioPosicaoRepository = usuarioPosicaoRepository;
+        }
+
+        public void Run()
+        {
+            var teste = _usuarioPosicaoRepository.QueryFilter("08309184778");
+            
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://zpnseqco:7cUFexcS37KfRZ0w0Q2F7PZYYJ-nWJMs@beaver.rmq.cloudamqp.com/zpnseqco")
+            };
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            bool durable = true;
+            bool exclusive = false;
+            bool autoDelete = false;
+
+            Dictionary<string, object> args = new Dictionary<string, object>()
+            {
+                { "x-queue-mode", "lazy" }
+            };
+
+            channel.QueueDeclare("UsuarioPosicaoShared", durable, exclusive, autoDelete, args);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($"Message received: {message}");
+            };
+
+            channel.BasicConsume(queue: "UsuarioPosicaoShared", autoAck: true, consumer: consumer);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Layer.Workers/Program.cs
@@ -0,0 +1,33 @@
+﻿using Layer.Domain.Settings;
+using Layer.Repository;
+using Layer.Repository.Context;
+using Layer.Repository.Interfaces;
+using Layer.Services;
+using Layer.Services.Base;
+using Layer.Services.Interfaces;
+using Layer.Workers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var configuration = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json")
+.Build();
+
+var mongoDbSettings = configuration.GetSection("MongoDatabase").Get<MongoDBSetting>();
+var connectionFactory = new ConnectionFactory(mongoDbSettings.ConnectionString);
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IUsuarioRepository>(p => new UsuarioRepository(connectionFactory, mongoDbSettings.DatabaseName, MongoDBCollections.CNUsuarios.ToString()));
+services.AddSingleton<IUsuarioPosicaoRepository>(p => new UsuarioPosicaoRepository(connectionFactory, mongoDbSettings.DatabaseName, MongoDBCollections.CNUsuariosPosicao.ToString()));
+services.AddSingleton<ITendenciaRepository>(p => new TendenciaRepository(connectionFactory, mongoDbSettings.DatabaseName, MongoDBCollections.CNTendencias.ToString()));
+
+services.AddTransient<IUsuarioService, UsuarioServices>();
+services.AddTransient<IUsuarioPosicaoService, UsuarioPosicaoServices>();
+services.AddTransient<ITendenciaService, TendenciaServices>();
+
+services.AddTransient<ConsoleApp>();
+
+services.BuildServiceProvider()
+    .GetService<ConsoleApp>()!.Run();
